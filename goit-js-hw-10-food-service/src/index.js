bcc2d00814f78b2eb1c684a713fca6d8beb03dbe
Chaiko85 <y.chaiko85@gmail.com>
2020-05-24 16:52:51 +0300

    HW№10

---
+++ goit-js-hw-10-food-service/src/index.js
@@ -0,0 +1,36 @@
+import './styles.css';
+import ourMenu from './templates/ourItemTemplate.hbs';
+import menu from './menu.json';
+
+document.querySelector('.js-menu').innerHTML = ourMenu(menu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const switchTheme = () => {
+  const body = document.querySelector('body');
+  if (body.classList.value === Theme.LIGHT) {
+    body.classList.remove(Theme.LIGHT);
+    body.classList.add(Theme.DARK);
+  } else {
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+  }
+};
+
+const jsSwitch = document.querySelector('.js-switch-input');
+jsSwitch.addEventListener('change', e => {
+  switchTheme();
+  if (e.target.checked) {
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+});
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  jsSwitch.checked = true;
+  switchTheme();
+}
