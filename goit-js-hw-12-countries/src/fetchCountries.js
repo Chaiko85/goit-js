7bc4941e16a1d7d875aa8d1b33b416252bec4240
Chaiko85 <y.chaiko85@gmail.com>
2020-06-21 23:35:23 +0300

    HW№12

--- goit-js-hw-12-countries/src/fetchCountries.js
+++ goit-js-hw-12-countries/src/fetchCountries.js
@@ -1,18 +1,8 @@
+const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return false;
-      }
-    })
-    .catch(() => {
-      return false;
-    });
+  const enteredCountry = baseUrl + searchQuery;
+  return fetch(enteredCountry).then(response => {
+    return response.json();
+  });
 }
